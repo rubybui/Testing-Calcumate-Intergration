380b534c754ec8f6241f83d7e0e7476b647c64c0
Ruby Bui <ruby@coderpush.com>
2022-10-26 19:33:26 +0700

    Calcumate

---
+++ pages/Calcumate/Calcumate.tsx
@@ -0,0 +1,13 @@
+const Calcumate = () => {
+  return (
+    <div
+      id="calcumate-root"
+      data-integration="www.e5f7-59-153-220-96.ap.ngrok.io"
+      data-integration-2=""
+      data-ref="AQICAHhReAOexSsfBeJ/FMNVdjLFzyMEhuepXqwRfRaqckbrnAHVm10RdYHQRjm5EceBt5jQAAAAgzCBgAYJKoZIhvcNAQcGoHMwcQIBADBsBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDPjE4zeOZHWsqaNmKwIBEIA/7pZGRKsaogV7gIfsJ1be4nLIDB2bNIsttuom5IfH2Q7y/4kZoaDa/1HZ/x4U/Fi4JeThooFm2kR1ygInVkPs"
+      data-int="5b8e0a12-0094-423c-8068-69aa227af382"
+    ></div>
+  );
+};
+
+export default Calcumate;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Script from "next/script";
+import Calcumate from "./Calcumate/Calcumate";
 
 export default function Home() {
   return (
@@ -13,45 +15,23 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Step to build your own calcumate calculator
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
+        <ul>
+          <li>Go to <a href="https://www.calcumate.co" target="_blank"> calcumate.co</a></li>
+          <li>Sign up for a free Account</li>
+          <li>Use ngrok to expose a local development server to the Internet.</li>
+          <li>Create a calculator at last step you would get your own calculator to add to Calcumate.tsx</li>
+        </ul>
+        <h1 className={styles.title}>Should show a view below.</h1>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Calcumate />
         </div>
+        <Script
+          src="https://calcumate-calculator-new-production.s3-ap-southeast-2.amazonaws.com/static/js/main.js"
+          strategy="afterInteractive"
+          defer
+        />
       </main>
 
       <footer className={styles.footer}>
@@ -60,12 +40,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
